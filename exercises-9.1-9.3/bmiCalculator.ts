ca43f8e70782f7a0a1150ec18595867283d017ef
Hoang Cao <cnhhoang850@gmail.com>
2020-10-28 11:13:23 +0700

    finish exercise 9.1-9.3

---
+++ exercises-9.1-9.3/bmiCalculator.ts
@@ -0,0 +1,44 @@
+type bmiCategories =
+  | "Normal (healthy weight)"
+  | "Skinny (unhealthy weight)"
+  | "Obese (unhealthy weight)";
+
+interface bodyIndexes {
+  height: number;
+  weight: number;
+}
+
+const parseArguments = (args: Array<string>): bodyIndexes => {
+  if (args.length < 4) throw new Error('Not enough arguments');
+  if (args.length > 4) throw new Error('Too many arguments');
+
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return {
+      height: Number(args[2]),
+      weight: Number(args[3])
+    }
+  } else {
+    throw new Error('Provided values were not numbers!');
+  }
+}
+
+
+
+const bmiCalculate = (height: number, weight: number): bmiCategories => {
+  const BMI = weight/(height/100*height/100);
+
+  if (BMI >= 25) {
+    return "Obese (unhealthy weight)";
+  } else if (BMI < 25 && BMI > 15) {
+    return "Normal (healthy weight)";
+  } else if (BMI <= 15) {
+    return "Skinny (unhealthy weight)"
+  }
+}
+
+try {
+  const {height, weight} = parseArguments(process.argv)
+  console.log(bmiCalculate(height, weight))
+} catch(e) {
+  console.log('Error, something bad happened, message: ', e.message);
+}
---
+++ exercises-9.1-9.3/exerciseCalculator.ts
@@ -0,0 +1,86 @@
+type exerciseSchedule = Array<number>;
+type rating = |1|2|3;
+const ratingDescriptions = [
+  "You passed your goals",
+  "You almost passed, not bad",
+  "You can try harder next time"
+]
+
+interface exerciseValues {
+  target: number;
+  schedule: Array<number>;
+}
+
+interface exerciseRating {
+  periodLength: number;
+  trainningDays: number;
+  success: boolean;
+  rating: number;
+  ratingDescription: string;
+  target: number;
+  average: number
+}
+
+const parseExerciseArguments = (args: Array<string>): exerciseValues => {
+  if (args.length < 4) throw new Error('Not enough arguments');
+  for (let i = 3; i < args.slice(3, -1).length; i++) {
+    if (isNaN(Number(args[i]))) {
+      throw new Error(`The ${i+1}th element in the schedule is not a numeric value`)
+      return
+    }
+  }
+  return {
+    target: Number(args[2]),
+    schedule: args.slice(2,-1).map(value => Number(value))
+  }
+}
+
+
+const calculateExercises = (
+  target: number,
+  schedule: exerciseSchedule,
+  ratingDescriptions: Array<string>
+): exerciseRating => {
+  const periodLength = schedule.length;
+
+  let trainningDays = 0;
+  let totalTrainningTime = 0
+  for (let i = 0; i < periodLength; i++) {
+    if (schedule[i] === 0) {
+      continue
+    } else {
+      ++trainningDays
+      totalTrainningTime += schedule[i]
+    }
+  };
+  const average = totalTrainningTime/trainningDays
+
+  let rating;
+  if (average === target || average > target) {
+    rating = 1;
+  } else if (Math.round(average*10)/10 === target) {
+    rating = 2;
+  } else {
+    rating = 3;
+  }
+
+  const ratingDescription = ratingDescriptions[rating - 1]
+  const success = rating === 3 ? false: true;
+
+  return {
+    periodLength,
+    trainningDays,
+    success,
+    rating,
+    ratingDescription,
+    target,
+    average
+  }
+}
+
+try {
+  const {target, schedule} = parseExerciseArguments(process.argv)
+  console.log(calculateExercises(target, schedule, ratingDescriptions))
+} catch(e) {
+  console.log('Error, something bad happened, message: ', e.message);
+}
